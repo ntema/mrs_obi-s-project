c5371b00472bebdad3d0ce8b709ddf2d59ba77b4
unknown <emmanuelntekim000@gmail.com>
2023-04-09 21:07:37 +0100

    ...

--- src/config/constants.js
+++ src/config/constants.js
@@ -7,9 +7,8 @@ module.exports = {
   ADMIN_TOKEN_SECRET: "NUD73839943,3/389287736633629H3JHHDJD5644EDKKD-D",
   REFRESH_TOKEN_SECRET:
     "JJDKKDIID89JJDHD6432243MMCHNJCMDDSCNS,NCJD78232;3NJNFSKK",
-  MAIL: "lanterncash@gmail.com",
-  // MAIL_PASS: "LCashSSL123&",
-  MAIL_PASS: "wmfowrqcvrobqsil",
+  MAIL: "emmanuelntekim000@gmail.com",
+  MAIL_PASS: "chzkkabgehbeauxs",
   mySecretKey: "FLWSECK-39f3e9e851b91068fc45aa9e1014afc1-X",
   TOKEN_EXPIRATION_TIME: "24h",
   PORT: 3000,
